982b54edda8ffc6547a778602f8b14f7d8efb16a
Júlia Küchle <juliagkuchle@gmail.com>
2023-10-16 15:41:30 -0300

    add app.ts and dbConfig

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+import { createDbConnection } from "./db/dbConfig";
+
+dotenv.config();
+
+createDbConnection();
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(express.json());
+
+app.use("/users", usersRouter);
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
---
+++ src/db/dbConfig.ts
@@ -0,0 +1,24 @@
+const sqlite3 = require("sqlite3").verbose();
+const filePath = "./src/db/events.db";
+
+let db: any = null;
+
+const createDbConnection = () => {
+    db = new sqlite3.Database(filePath, (error: any) => {
+        if (error) {
+            return console.error(error.message);
+        }
+    });
+    console.log("Connection with SQLite has been estabilished");
+    createTableUsers();
+    return db;
+};
+
+const createTableUsers = () => {
+    db.exec(`CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nome VARCHAR(50),
+    );
+    `);
+};
+export { createDbConnection, db };
