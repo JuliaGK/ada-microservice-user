cb4ace47d1d6bb5041939efcbabf14b7023d9f0f
Júlia Küchle <juliagkuchle@gmail.com>
2023-10-25 15:50:14 -0300

    add cors restriction

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { createDbConnection } from "./db/dbConfig";
 import usersRouter from "./routes/usersRoute";
+import cors from "cors";
 
 dotenv.config();
 
@@ -11,6 +12,8 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
+
+app.use(cors({ origin: ["http://localhost:4000"] }));
 
 app.use("/users", usersRouter);
 
