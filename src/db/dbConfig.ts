f111265d833609974e0e55a5e1371d38b5ff3de5
Júlia Küchle <juliagkuchle@gmail.com>
2023-11-06 14:33:38 -0300

    add test database

--- src/db/dbConfig.ts
+++ src/db/dbConfig.ts
@@ -2,7 +2,10 @@ import sqlite3 from "sqlite3";
 import { open } from "sqlite";
 
 const initializeDatabase = async () => {
-    const dbFilename = "./src/db/database.sqlite";
+    const dbFilename =
+        process.env.NODE_ENV === "test"
+            ? "./src/db/test_database.sqlite"
+            : "./src/db/database.sqlite";
 
     return open({
         filename: dbFilename,
