07b7271371f2bcbc10444b910bea1839c629417d
Júlia Küchle <juliagkuchle@gmail.com>
2023-11-06 15:46:51 -0300

    add getUser test

--- src/controllers/userController.test.ts
+++ src/controllers/userController.test.ts
@@ -1,5 +1,15 @@
 import { expect, test, beforeEach } from "vitest";
 import { initializeDatabase } from "../db/dbConfig";
+import { faker } from "@faker-js/faker";
+import User from "../models/User";
+import { getUserHandler } from "./usersController";
+
+const makeUser = async (user: User) => {
+    const db = await initializeDatabase();
+    const id = user.id ?? faker.number.int();
+
+    await db.run("INSERT INTO users (id, nome) VALUES (?, ?)", [id, user.nome]);
+};
 
 beforeEach(async () => {
     const dbPromise = initializeDatabase();
@@ -8,4 +18,21 @@ beforeEach(async () => {
     await db.run("DELETE FROM users");
 });
 
-test("test if user is added");
+test("test if i can get an user", async () => {
+    const nome = faker.lorem.words(3);
+
+    const userToCreate: User = {
+        id: faker.number.int().toString(),
+        nome,
+    };
+
+    await makeUser(userToCreate);
+
+    const user = await getUserHandler(userToCreate.id);
+
+    expect(user).toEqual(
+        expect.objectContaining({
+            nome,
+        })
+    );
+});
--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -43,3 +43,5 @@ export const usersController = {
         }
     },
 };
+
+export { getUserHandler };
