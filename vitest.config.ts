2c4684b3ce1a27eadb487605eae53d44282d354e
Júlia Küchle <juliagkuchle@gmail.com>
2023-11-07 13:16:59 -0300

    add vitest config for coverage

---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    test: {
+        coverage: {
+            enabled: true,
+            include: ["src/controllers/**"],
+            // opcional
+            provider: "v8",
+        },
+    },
+});
